820c01e4c78261a069bf57b934b7a4b1ef677ba8
MaraAebicher <marapelosoaebicher@gmail.com>
2021-12-03 13:12:13 -0300

    Segundo commit

--- ProyectoG4/urls.py
+++ ProyectoG4/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('g4/', views.Grupo4, name = 'primera_vista'),
 ]
+
+#PATH QUE APUNTA A APPS
+
+path('Principal/', include('apps.Principal.urls'))
---
+++ ProyectoG4/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def Grupo4(request):
+	return render(request,'mi_html.html')
